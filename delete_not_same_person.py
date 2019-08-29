093394b8123adbeef6fb4f67bf797a21b3499a96
夜雨飘零 <yeyupiaoling@foxmail.com>
2019-08-29 14:59:14 +0800

    Update delete_not_same_person.py

--- delete_not_same_person.py
+++ delete_not_same_person.py
@@ -7,11 +7,14 @@ import face_recognition
 # 判断是否以及标记正确类别的图片
 def check_if_all_rename(father_path, name_paths):
     exist = True
+    i = 0
     for name_path in name_paths:
         main_image = os.path.join(father_path, name_path, '0.jpg')
         if not os.path.exists(main_image):
             shutil.rmtree(os.path.join(father_path, name_path))
+            i += 1
             print('%s 还没标记`0.jpg`图片' % name_path)
+    print(i)
     return exist
 
 
@@ -25,8 +28,12 @@ if __name__ == '__main__':
             image_paths = os.listdir(os.path.join(father_path, name_path))
             # 正确图片的路径
             main_image = os.path.join(father_path, name_path, '0.jpg')
-            main_img = face_recognition.load_image_file(main_image)
-            main_encodings = face_recognition.face_encodings(main_img, num_jitters=10)[0]
+            try:
+                main_img = face_recognition.load_image_file(main_image)
+                main_encodings = face_recognition.face_encodings(main_img, num_jitters=10)[0]
+            except:
+                shutil.rmtree(os.path.join(father_path, name_path))
+                continue
             for image_path in image_paths:
                 # 要对比的图片
                 img_path = os.path.join(father_path, name_path, image_path)
